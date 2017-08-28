ed38ddbbf2d8bef94d945506892a44184e1e0a3d
Mrdilbaz <mrdilbaz@gmail.com>
2017-08-28 20:08:48 +0300

    new regions

--- resources/views/sayfalar/hamkayit_duzenle.blade.php
+++ resources/views/sayfalar/hamkayit_duzenle.blade.php
@@ -3,29 +3,35 @@
 @section('content')
   <div class='container'>
     <div class='form-group'><input class="form-control form-control-lg" type="text" name='isim' value='{{$kayit->isim}}'></div>    
-    <div id='waveform' style='height:130px;overflow-x:visible;'></div>
-    <div id="waveform-timeline"></div>
-    <div style="text-align: center">
-        <button class="btn btn-primary" onclick="wavesurfer.playPause()">
-            <i class="glyphicon glyphicon-play"></i>
-            Play
-        </button>
-
-        <p class="row">
-            <div class="col-xs-1">
-            <i class="glyphicon glyphicon-zoom-in"></i>
-            </div>
-
-            <div class="col-xs-10">
-            <input id="slider" type="range" min="1" max="10" value="1" style="width: 100%" />
-            </div>
-
-            <div class="col-xs-1">
-            <i class="glyphicon glyphicon-zoom-out"></i>
-            </div>
-        </p>
+    <div class='row justify-content-end no-gutters pb-2'>
+        <div class='mr-auto'>
+        
+            <button id='play-button' class="btn btn-success btn-sm" onclick="wavesurfer.playPause()" >
+                <i class="fa fa-play"></i>
+                Oynat
+            </button>
+        
+        </div>
+        <div class='px-2'>
+            <button class='btn btn-info btn-sm' style='width:80px;' onclick="zoomIn()">
+                <i class="fa fa-search-plus" aria-hidden="true"></i>
+                Yaklaş
+            </button>
+        </div>
+        <div class=''>
+            <button class='btn btn-info btn-sm' style='width:80px;' onclick="zoomOut()">
+                <i class="fa fa-search-minus" aria-hidden="true"></i>
+                Uzaklaş
+            </button>
+        </div>
     </div>
-
+    <div class='container' style='height:150px'>
+        <div id='wave-loading' style='position:absolute;text-align:center;width:100%;top:2.5em'><h2 class='text-muted font-italic'>Yükleniyor</h2></div>
+        <div id='waveform'></div>
+        <div id="waveform-timeline"></div>
+    </div>
+    <div class='row justify-content-center'>
+        
   </div>
 @endsection
 
@@ -37,19 +43,19 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/1.2.3/plugin/wavesurfer.regions.min.js"></script>
 
     <script>
+        
         var wavesurfer = WaveSurfer.create({
             container: '#waveform',
             waveColor: '#22963b',
             progressColor: '#06591c',
-            scrollParent: true,
-            minPxPerSec:1,
-            renderer:'MultiCanvas',
-
+            normalize:true
         });
+
         wavesurfer.load('{{ asset($kayit->dosya) }}');
         
 
         wavesurfer.on('ready', function () {
+            $('#wave-loading').hide();
             var timeline = Object.create(WaveSurfer.Timeline);
 
             timeline.init({
@@ -67,11 +73,17 @@
             });
         });
 
-        var slider = document.querySelector('#slider');
+        var currentZoom = 0.1;
+        var zoomSensitivity = 0.5;
 
-        slider.oninput = function () {
-            var zoomLevel = Number(slider.value);
-            wavesurfer.zoom(zoomLevel);
-        }; 
+        function zoomIn(){
+            currentZoom = Math.min(currentZoom + zoomSensitivity,50);
+            wavesurfer.zoom(currentZoom);
+        }
+
+        function zoomOut(){
+            currentZoom = Math.max(currentZoom - zoomSensitivity,0.1);
+            wavesurfer.zoom(currentZoom);
+        }
     </script>
 @endsection
